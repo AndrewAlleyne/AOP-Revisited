f6f78083ab8bee2f7e6a31e62202e6b6ac716017
Andrew Alleyne <andrew.alleyne.jr@gmail.com>
2023-03-08 14:50:58 -0500

    Spring AOP basics

---
+++ src/test/java/com/example/aoprevisited/AopRevisitedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aoprevisited;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopRevisitedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
